e0725c9d5adc78015897a10c96405844c6056797
Emma Bright <113352515+emmabrght@users.noreply.github.com>
2023-07-05 14:08:57 +0200

    a

--- contact.php
+++ contact.php
@@ -6,7 +6,6 @@ require '/vendor/phpmailer/phpmailer/src/Exception.php';
 require '/vendor/phpmailer/phpmailer/src/PHPMailer.php';
 require '/vendor/phpmailer/phpmailer/src/SMTP.php';
 
-// Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $email = $_POST['email'];
@@ -16,7 +15,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mail = new PHPMailer(true);
 
     try {
-        // Configure SMTP settings
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
@@ -25,16 +23,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->SMTPSecure = 'tls';
         $mail->Port = 587;
 
-        // Set sender and recipient
         $mail->setFrom($email, $name);
         $mail->addAddress('YOUR_EMAIL_ADDRESS');
 
-        // Set email content
         $mail->isHTML(false);
         $mail->Subject = 'Contact Form Submission: ' . $topic;
         $mail->Body = "Name: $name\n\nEmail: $email\n\nTopic: $topic\n\nMessage: $message";
 
-        // Send the email
         $mail->send();
 
         echo 'Thank you for contacting us. We will get back to you soon!';
